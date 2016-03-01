59e76a448e30f2899643989248ef02b9669e9c90
tonyatoms <tony@atoms.net>
2016-03-01 15:59:44 -0600

    just a pre-breaking things commit - now fetches actual (TEST) report data though!

--- index.ios.js
+++ index.ios.js
@@ -6,46 +6,83 @@
 import React, {
   AppRegistry,
   Component,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_REPORT_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+var REQUEST_URL = 'http://311api.cityofchicago.org/open311/v2/requests.json?service_code=4fd3b167e750846744000005'
+
 class AwesomeProject extends Component {
+// add some initial state
+  constructor(props) {
+    super(props);
+    this.state = {
+      reports: null,
+    };
+  }
+  componentDidMount() {
+      this.fetchData();
+  }
+  fetchData() {
+   fetch(REQUEST_URL)
+     .then((response) => response.json())
+     .then((responseData) => {
+       this.setState({
+         reports: responseData,
+       });
+     })
+     .done();
+ }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Mender
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+  var reports = MOCKED_REPORT_DATA[0];
+
+  //console.warn(this.state.reports);
+
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{uri: reports.posters.thumbnail}}
+        style={styles.thumbnail}
+      />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{reports.title}</Text>
+          <Text style={styles.year}>{reports.year}</Text>
+        </View>
       </View>
     );
   }
-}
+} // end of class AwesomeProject
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
+
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    rightContainer: {
+    flex: 1,
   },
-  instructions: {
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+    title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
